6ae7a5b8f1388e46bb918abcdd6316a84cf0b363
Ludvig Carlsen <ludvigmcarlsen@hotmail.com>
2020-12-06 15:13:04 +0100

    Day 6 complete

--- adventofcode.py
+++ adventofcode.py
@@ -673,12 +673,167 @@ def day5():
     print(f"5.2. The ID of my seat is {current-1}")
 
 
+def day6():
+
+    """Day 6: Custom Customs
+
+    As your flight approaches the regional airport where you'll switch to a much
+    larger plane, customs declaration forms are distributed to the passengers.
+
+    The form asks a series of 26 yes-or-no questions marked a through z. All you
+    need to do is identify the questions for which anyone in your group answers
+    "yes". Since your group is just you, this doesn't take very long.
+
+    However, the person sitting next to you seems to be experiencing a language
+    barrier and asks if you can help. For each of the people in their group, you
+    write down the questions for which they answer "yes", one per line. For
+    example:
+
+    abcx
+    abcy
+    abcz
+
+    In this group, there are 6 questions to which anyone answered "yes": a, b,
+    c, x, y, and z. (Duplicate answers to the same question don't count extra;
+    each question counts at most once.)
+
+    Another group asks for your help, then another, and eventually you've
+    collected answers from every group on the plane (your puzzle input). Each
+    group's answers are separated by a blank line, and within each group, each
+    person's answers are on a single line. For example:
+
+    abc
+
+    a
+    b
+    c
+
+    ab
+    ac
+
+    a
+    a
+    a
+    a
+
+    b
+
+    This list represents answers from five groups:
+
+        The first group contains one person who answered "yes" to 3 questions: a, b, and c.
+        The second group contains three people; combined, they answered "yes" to 3 questions: a, b, and c.
+        The third group contains two people; combined, they answered "yes" to 3 questions: a, b, and c.
+        The fourth group contains four people; combined, they answered "yes" to only 1 question, a.
+        The last group contains one person who answered "yes" to only 1 question, b.
+
+    In this example, the sum of these counts is 3 + 3 + 3 + 1 + 1 = 11.
+
+    For each group, count the number of questions to which anyone answered
+    "yes". What is the sum of those counts?
+    """
+
+    print("\n--- DAY 6 ---")
+
+    sum_questions = 0
+    questions = set()
+
+    def iterate_line(line):
+        for q in line.strip():
+            questions.add(q)
+
+    with open("data/day6.txt") as puzzle_input:
+        lines = puzzle_input.readlines()
+        for line in lines:
+            if line is lines[-1]:
+                iterate_line(line)
+
+            # End of group or last input line
+            if len(line.strip()) == 0 or line is lines[-1]:
+                sum_questions += len(questions)
+                questions.clear()
+
+            else:
+                iterate_line(line)
+
+    print(f"6.1. The total sum of answered questions is {sum_questions}")
+
+
+    """Part Two
+    As you finish the last group's customs declaration, you notice that you misread
+    one word in the instructions:
+
+    You don't need to identify the questions to which anyone answered "yes"; you
+    need to identify the questions to which everyone answered "yes"!
+
+    Using the same example as above:
+
+    abc
+
+    a
+    b
+    c
+
+    ab
+    ac
+
+    a
+    a
+    a
+    a
+
+    b
+
+    This list represents answers from five groups:
+
+        In the first group, everyone (all 1 person) answered "yes" to 3 questions: a, b, and c.
+        In the second group, there is no question to which everyone answered "yes".
+        In the third group, everyone answered yes to only 1 question, a. Since some people did not answer "yes" to b or c, they don't count.
+        In the fourth group, everyone answered yes to only 1 question, a.
+        In the fifth group, everyone (all 1 person) answered "yes" to 1 question, b.
+
+    In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+
+    For each group, count the number of questions to which everyone answered "yes".
+    What is the sum of those counts?
+    """
+
+    sum_questions = 0
+    group = set()
+
+    # Returns 1 if everyone answered, else 0
+    def compare(answer):
+        for p in group:
+            if answer not in p:
+                return 0
+        return 1
+
+    with open("data/day6.txt") as puzzle_input:
+        lines = puzzle_input.readlines()
+        for line in lines:
+            if line is lines[-1]:
+                group.add(line.strip())
+
+            if len(line.strip()) == 0 or line is lines[-1]:
+                for p in group:
+                    for a in p:
+                        sum_questions += compare(a)
+                    break # Avoids duplicate comparisons
+                group.clear()
+
+            else:
+                group.add(line.strip())
+
+    print(f"6.2. The total sum of answered questions is {sum_questions}")
+
+
+
 def main():
     #day1()
     #day2()
     #day3()
     #day4()
-    day5()
+    #day5()
+    day6()
 
 
 if __name__ == "__main__":
