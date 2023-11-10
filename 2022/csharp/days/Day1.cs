a40e1fba5670f1cec71d475ce3ead3051f3d687e
Ludvig Marcel Carlsen <ludvigmcarlsen@hotmail.com>
2023-11-10 09:06:45 +0100

    Day 1 task 1

--- 2022/csharp/days/Day1.cs
+++ 2022/csharp/days/Day1.cs
@@ -4,28 +4,57 @@ namespace Application.Days
     {
         private List<List<int>> input;
 
+    
         public Day1() : base(1) {
             input = GroupAdjacentNumbers();
         }
 
-
-
+    
         public override object Part1()
         {
-            // TODO: hjelp
+            List<int> listOfSums = new();
 
+            // Sum all groups of numbers            
+            for (int i = 0; i < input.Count; i++)
+            {
+                List<int> listOfNumbers = input[i];
+                int sum = SumNumbers(listOfNumbers);
+                listOfSums.Add(sum);
+            }
 
+            int largestSum = 0;
 
-            return null;
+            // Find the largest sum
+            for (int i = 0; i < listOfSums.Count; i++)
+            {
+                int sum = listOfSums[i];
+                
+                if (sum > largestSum) {
+                    largestSum = sum;
+                }
+            }
+
+            return largestSum;
         }
 
         public override object Part2()
         {
-
-
-
-
             return null;
         }
+
+
+
+         private int SumNumbers(List<int> numbers) {
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int number = numbers[i];
+                sum = sum + number;                
+            }
+
+            return sum;
+        }
+
     }
 }
